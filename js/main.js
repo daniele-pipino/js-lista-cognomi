5fcdf2cfc4244f7d3d799935e6cf208383f0fd2b
daniele-pipino <Danielemacpipino@icloud.com>
2021-07-06 14:58:53 +0200

    aggiunto ordinamento alfabetico

--- js/main.js
+++ js/main.js
@@ -10,13 +10,15 @@
 var list = document.getElementById('mainlist');
 
 // definizione array
-var cognomi = ['carbone', 'viapiana', 'massari', 'di bella', 'ribezzo'];
+var cognomi = ['carbone', 'viapiana', 'massari', 'donatiello', 'ribezzo'];
 console.table('prima: ', cognomi);
 
 // inseriemento cognome
 var request = prompt('Aggiungi il tuo cognome alla lista');
-cognomi.push = (request);
+cognomi.push = request;
 console.table('dopo: ', cognomi);
 
-// stampa su html
-var stampa = '';
+// ordinamento alfabetico
+for (var i = 0; i <= cognomi.length; i++) {
+    console.log(cognomi.sort()[i]);
+}
